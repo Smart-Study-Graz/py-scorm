abbcefd61019fa625511c3d7195c6880f00daff5
Philipp Hafner <phi.hafner@gmail.com>
2022-02-17 10:47:07 +0100

    fixed dist

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 from setuptools import find_packages
 
+def _find_packages():
+    packages = find_packages(where='.')
+    #packages.append('py_scorm.data')
+    return packages
+
 setup(
   name='py_scorm',
-  packages=find_packages('.'),
+  packages=_find_packages(),
   install_requires=[
     ],
+  # package_data={
+  #   'py_scorm': ['py_scorm/data/scorm_12/*']
+  # },
   include_package_data=True,
+  # package_dir={
+  #           'py_scorm.data': 'data',
+  #       },
 )
